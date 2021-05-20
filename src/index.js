e1b6cf831720795aee6b1d68babae3ba418dfed5
Pedro Sanchez Hinostroza <pedro.sanhin@gmail.com>
2021-05-20 15:07:58 -0500

    Hello world with React.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = <h1>Hello, Platzi Badges!</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
