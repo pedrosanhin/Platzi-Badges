f2388bb430308b5f2c2532bf0019a258dfcb7561
Pedro Sanchez Hinostroza <pedro.sanhin@gmail.com>
2021-05-24 14:04:23 -0500

    Added pages BadgeNew and Navbar and added all styles for Navbar

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './styless/Badge.css';
-import confLogo from '../images/badge-header.svg';
+import "./styless/Badge.css";
+import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
@@ -12,17 +12,19 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img 
-          className="Badge__avatar"
-          src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <img
+            className="Badge__avatar"
+            src={this.props.avatarUrl}
+            alt="Avatar"
+          />
           <h1>
-            Pedro <br /> Sanchez
+            {this.props.firstName} <br /> {this.props.lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Software Engineer</h3>
-          <div>@pedroA1007</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">#platziconf</div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./styless/Navbar.css";
+import logo from "../images/logo.svg";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <div className="container-fluid">
+          <a className="Navbar__brand" href="/">
+            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+            <span className="fw-light">Platzi</span>
+            <span className="fw-bold">Conf</span>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
