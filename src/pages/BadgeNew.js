dbbf000d23f8c32fb0a1c1ff5d555f213d5a255c
Pedro Sanchez Hinostroza <pedro.sanhin@gmail.com>
2021-05-25 17:36:12 -0500

    Added functionality for the Badge and BadgeForm to change in real time

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,34 @@ import BadgeForm from "../components/BadgeForm";
 import Navbar from "../components/Navbar";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
+
+  handleChange = (e) => {
+    //Shit is another form to do it
+    // const nextForm = this.state.form;
+    // nextForm[e.target.name] = e.target.value;
+    // this.setState({
+    //   form: {
+    //     [e.target.name]: e.target.value,
+    //   },
+    // });
+
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -19,16 +47,20 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col">
               <Badge
-                firstName="Pedro"
-                lastName="Sanchez"
-                twitter="pedroA1007"
-                jobTitle="Software Engineer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://www.gravatar.com/avatar?d=identicon"
               />
             </div>
 
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
