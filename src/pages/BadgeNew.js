11158316398e90f1371a4c686ff900f005dcfa28
Pedro Sanchez Hinostroza <pedro.sanhin@gmail.com>
2021-05-25 12:47:27 -0500

    Added BadgeForm and some methods for the events on it

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
-import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+import Navbar from "../components/Navbar";
 
 class BadgeNew extends React.Component {
   render() {
@@ -24,6 +25,10 @@ class BadgeNew extends React.Component {
                 jobTitle="Software Engineer"
                 avatarUrl="https://www.gravatar.com/avatar?d=identicon"
               />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
