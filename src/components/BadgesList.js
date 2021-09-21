cf2c20316bbcf81910388106de858ec16aa3b86e
Pedro Sanchez Hinostroza <pedro.sanhin@gmail.com>
2021-09-21 11:00:24 -0500

    Handling all sates of the API calls: succes, error and no data

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles/BadgesList.css";
 import "./styles/Badge.css";
@@ -6,6 +7,17 @@ import twitterLogo from "../images/twitter-logo.ico";
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
+    }
+
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,90 +5,44 @@ import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList";
 
+import api from "../api";
+
 class Badges extends React.Component {
   constructor(props) {
     super(props);
-    console.log("1. contructor()");
 
     this.state = {
-      data: [],
+      loading: true,
+      error: null,
+      data: undefined,
     };
   }
 
   componentDidMount() {
-    console.log("3. componentdidMount()");
-
-    this.timeoutId = setTimeout(() => {
-      this.setState({
-        data: [
-          {
-            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-            firstName: "Pedro",
-            lastName: "Sanchez",
-            email: "pedro.sanhin@gmail.com",
-            jobTitle: "Software Engineer",
-            twitter: "pedroA1007",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon",
-          },
-          {
-            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-            firstName: "Viry",
-            lastName: "Guitierrez",
-            email: "viri@hotmail.com",
-            jobTitle: "Data Scientist",
-            twitter: "virygut",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon",
-          },
-          {
-            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-            firstName: "Efraim",
-            lastName: "Olague",
-            email: "elefraol@hotmail.com",
-            jobTitle: "Backend Developer",
-            twitter: "efrasm",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon",
-          },
-          {
-            id: "64c03386-33a2-4512-9ac1-354ad7ebc5e9",
-            firstName: "Andres",
-            lastName: "Muro",
-            email: "andym@hotmail.com",
-            jobTitle: "Frontend Developer",
-            twitter: "andreeeees",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/f9879d71855b5ff21e4963273a886bfc?d=identicon",
-          },
-        ],
-      });
-    }, 3000);
-
-    //clearTimeout(timeoutId);//sirve para cancelar los timeouts que quedaron pendientes
-    //Pero esto se hace en el componenteWillUnmount
+    //Este es el mejor lugar para hacer la peticion de una API
+    this.fetchData();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("5. componentDidUpdate()");
-    console.log({
-      prevProps: prevProps,
-      prevState: prevState,
-    });
-    console.log({
-      props: this.props,
-      state: this.state,
-    });
-  }
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
 
-  componentWillUnmount() {
-    console.log("6. componentWillUnmount()");
-
-    clearTimeout(this.timeoutId);
-  }
+    try {
+      const data = await api.badges.list();
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
 
   render() {
-    console.log("2. render()");
+    if (this.state.loading === true) {
+      return "Loading...";
+    }
+
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}`;
+    }
+
     return (
       <React.Fragment>
         <div className="Badges">
